c0c98c8c9cdbd80d923ec0df168dbc38dacd7162
Theus <matheusth.dev@gmail.com>
2022-11-23 01:10:45 -0300

    feat: front end base and full dockerization

---
+++ frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <>
+    <h1>File Request</h1>
+  </>
+);
